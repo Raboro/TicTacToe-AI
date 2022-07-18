e323d274795e2759bbc96c5167fb87d1a8af06b2
Raboro <raborogit@gmail.com>
2022-07-18 11:31:02 +0200

    finished Ai test

--- src/Tests/TestAi.java
+++ src/Tests/TestAi.java
@@ -1,5 +1,65 @@
 package Tests;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import TicTacToeAI.Ai;
+import TicTacToeAI.Game;
+
 public class TestAi {
-    
+
+    @Test
+    public void TestAiMoveX0Y0() {
+
+        String[][] result = {
+                { "X", " ", " " },
+                { " ", " ", " " },
+                { " ", " ", " " }
+        };
+
+        Ai.bestMove();
+        for (int x = 0; x < 3; x++) {
+            for (int y = 0; y < 3; y++) {
+                Assert.assertEquals(result[x][y], Game.board[x][y]);
+            }
+        }
+    }
+
+    @Test
+    public void TestAiMoveX0Y1() {
+
+        String[][] result = {
+                { "X", "X", " " },
+                { " ", "O", " " },
+                { " ", " ", " " }
+        };
+
+        Game.board[1][1] = Game.human;
+
+        Ai.bestMove();
+        for (int x = 0; x < 3; x++) {
+            for (int y = 0; y < 3; y++) {
+                Assert.assertEquals(result[x][y], Game.board[x][y]);
+            }
+        }
+    }
+
+    @Test
+    public void TestAiMoveX2Y0() {
+
+        String[][] result = {
+                { "X", "X", "O" },
+                { " ", "O", " " },
+                { "X", " ", " " }
+        };
+
+        Game.board[0][2] = Game.human;
+
+        Ai.bestMove();
+        for (int x = 0; x < 3; x++) {
+            for (int y = 0; y < 3; y++) {
+                Assert.assertEquals(result[x][y], Game.board[x][y]);
+            }
+        }
+    }
 }
